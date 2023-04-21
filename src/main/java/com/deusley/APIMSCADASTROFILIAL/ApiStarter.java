74bba5c95373c75a1f2157286bc86c521168eb40
Deuslleyw <94429495+Deuslleyw@users.noreply.github.com>
2023-04-21 18:00:28 -0300

    FirstCommit

---
+++ src/main/java/com/deusley/APIMSCADASTROFILIAL/ApiStarter.java
@@ -0,0 +1,13 @@
+package com.deusley.APIMSCADASTROFILIAL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiStarter {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiStarter.class, args);
+	}
+
+}
